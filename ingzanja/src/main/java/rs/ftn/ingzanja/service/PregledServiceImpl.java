5cf6857872153bcba3aad1736c235c28e054cb92
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-06-02 23:46:42 +0200

    added new method on PregledService

--- ingzanja/src/main/java/rs/ftn/ingzanja/service/PregledServiceImpl.java
+++ ingzanja/src/main/java/rs/ftn/ingzanja/service/PregledServiceImpl.java
@@ -2,10 +2,17 @@ package rs.ftn.ingzanja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rs.ftn.ingzanja.dto.PregledDTO;
 import rs.ftn.ingzanja.model.Pacient;
 import rs.ftn.ingzanja.model.Pregled;
+import rs.ftn.ingzanja.model.Simptom;
 import rs.ftn.ingzanja.repository.PacientRepository;
 import rs.ftn.ingzanja.repository.PregledRepository;
+import rs.ftn.ingzanja.repository.SymptomsRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class PregledServiceImpl implements PregledService {
@@ -16,8 +23,21 @@ public class PregledServiceImpl implements PregledService {
     @Autowired
     PacientRepository pacientRepo;
 
+    @Autowired
+    SymptomsRepository symptomRepo;
+
     @Override
-    public void savePregledOnly(Pregled p) {
+    public void savePregledOnly(Pregled p, PregledDTO body) {
+
+        Set<Simptom> simptoms = new HashSet<>();
+        for(String s : body.getSimptoms())
+        {
+            Simptom simptom = symptomRepo.findOneByNaziv(s);
+            if(simptom != null)
+                simptoms.add(simptom);
+        }
+        p.setSimptoms(simptoms);
+        repository.save(p);
 
     }
 
